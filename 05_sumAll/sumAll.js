056c3071b53dc6fa54b1f054ae06835262efb90b
Nakul Asus <69424522+NakulKamath@users.noreply.github.com>
2024-01-27 13:26:05 +0530

    Solved Q5, all tests passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(startInt, endInt) {
+    if (startInt<0||endInt<0||typeof (startInt+endInt) != "number") {
+        return 'ERROR';
+    }
+    if (startInt > endInt) {
+        let temp = startInt;
+        startInt = endInt;
+        endInt = temp;
+    }
+    let sum = 0;
+    for (let i = startInt; i <= endInt; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
