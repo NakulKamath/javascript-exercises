981b98e9dc00060ae61b98b1ef33f236e03cbe0b
Nakul Asus <69424522+NakulKamath@users.noreply.github.com>
2024-01-27 13:25:38 +0530

    Solved Q4, all tests passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...elements) {
+    // return array.filter((value) => {return !elements.includes(value)});
+    let newarray = [];
+    array.forEach((element) => {
+        if (!elements.includes(element)) {
+            newarray.push(element);
+        }
+    });
+    return newarray;
 };
 
 // Do not edit below this line
