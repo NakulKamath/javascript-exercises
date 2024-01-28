96c2b8dd701f830747232ca26f776f7bd47e3efe
Nakul Asus <69424522+NakulKamath@users.noreply.github.com>
2024-01-28 23:03:33 +0530

    Solved Q12, all tests passed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const currentYear = new Date().getFullYear();
+    let oldestAge = 0;
+    let oldest;
+    for (let i = 0; i < array.length; i++) {
+        let yearOfDeath = array[i].yearOfDeath || currentYear;
+        let age = yearOfDeath - array[i].yearOfBirth;
 
+        if (age > oldestAge) {
+            oldestAge = age;
+            oldest = array[i];
+        }
+    }
+    return oldest;
 };
 
 // Do not edit below this line
