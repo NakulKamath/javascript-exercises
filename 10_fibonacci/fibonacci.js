8dda17095e7491ce37f7d0d777a46742ee2279a5
Nakul Asus <69424522+NakulKamath@users.noreply.github.com>
2024-01-28 22:28:47 +0530

    Solved Q10, all tests passed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(target) {
+    target = +target;
+    if (target==0) {
+        return 0;
+    } else if (target<0) {
+        return "OOPS";
+    }
 
+    let arr = [0, 1];
+    for (let i = 2; i <= target; i++) {
+        arr[i] = arr[i-2]+arr[i-1];
+    }
+    return arr[target];
 };
 
 // Do not edit below this line
